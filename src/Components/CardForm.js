b599f7ec351560e76156377d57444d7c5d870150
Patrice Tekeda <patricetekeda@gmail.com>
2022-12-17 23:28:19 -0500

    EditCard, CardForm and AddCard updated. components reused, form deleted

---
+++ src/Components/CardForm.js
@@ -0,0 +1,36 @@
+function CardForm({ card, handleSubmit, handleChange, handleCancel }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Front</label>
+        <textarea
+          id="front"
+          name="front"
+          className="form-control"
+          onChange={handleChange}
+          type="text"
+          value={card.front}
+        />
+      </div>
+      <div className="form-group">
+        <label>Back</label>
+        <textarea
+          id="back"
+          name="back"
+          className="form-control"
+          onChange={handleChange}
+          type="text"
+          value={card.back}
+        />
+      </div>
+      <button className="btn btn-secondary mx-1" onClick={() => handleCancel()}>
+        Cancel
+      </button>
+      <button className="btn btn-primary mx-1" type="submit">
+        Save
+      </button>
+    </form>
+  );
+}
+
+export default CardForm;
